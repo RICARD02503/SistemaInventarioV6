0bfc9a245469a41907184f4ef0e8c8bca0cebf6c
ricardoaperez <ricardoperezchavez196@gmail.com>
2026-01-28 19:55:07 -0500

    Configuracion del proyecto

---
+++ ClassLibrary1.AccesoDatos/Configuración/BodegaConfiguracion.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaInventario.AccesoDatos.Configuración
+{
+    public class BodegaConfiguracion : IEntityTypeConfiguration<Bodega>
+    {
+        public void Configure(EntityTypeBuilder<Bodega> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+            builder.Property(x => x.Nombre).IsRequired().HasMaxLength(60);
+            builder.Property(x => x.Descripcion).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Estado).IsRequired();
+        }
+    }
+}
--- SistemaInventarioV6/Areas/Inventario/Controllers/HomeController.cs
+++ SistemaInventarioV6/Areas/Inventario/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-using SistemaInventarioV6.Models;
+using SistemaInventario.Modelos.ViewModels;
 using System.Diagnostics;
 
-namespace SistemaInventarioV6.Controllers
+namespace SistemaInventarioV6.Areas.Inventario.Controllers
 {
+    [Area("Inventario")]
     public class HomeController : Controller
     {
         public IActionResult Index()
